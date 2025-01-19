537ac548db22f57b48271f295e43eb4d9e889685
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-19 13:13:43 -0600

    Add order producer service

---
+++ order-service/src/main/java/net/javaguides/orderservice/publisher/OrderProducer.java
@@ -0,0 +1,30 @@
+package net.javaguides.orderservice.publisher;
+
+import net.javaguides.orderservice.dto.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String orderRoutingKey;
+
+    private RabbitTemplate rabbitTemplate;
+
+    public OrderProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(Order order) {
+        LOGGER.info(String.format("Sending order event message to rabbitMQ -> %s", order));
+        rabbitTemplate.convertAndSend(exchange, orderRoutingKey, order);
+    }
+}
